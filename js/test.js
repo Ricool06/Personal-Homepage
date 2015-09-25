77da057d8e0b402e4fcba7bea95e8320121e05ea
Ryan Perry <ricoolohsix@hotmail.com>
2015-09-25 16:45:01 +0100

    +Reordered script to be more readable. +Added animation to background image. +Added placeholder "onLoad" function.

--- js/test.js
+++ js/test.js
@@ -1,14 +1,26 @@
 /**
  * @author Ryan Perry
  */
+function handleResult(result) {
+	maxSizeImgUrl = result.url.replace("1024","");
+	document.body.style.backgroundImage = "url(" + maxSizeImgUrl + ")";
+	document.body.style.backgroundRepeat = "no-repeat";
+	document.body.style.backgroundAttachment = "fixed";
+	document.body.style.backgroundSize = "cover";
+	document.body.style.animationName = "bg-transform";
+	document.body.style.animationDuration = "60s";
+	document.body.style.animationIterationCount = "infinite";
+};
 
+function onLoad() {
+	
+}
+function animateBg() {
+	
+}
 $.ajax({
 	url:"https://api.nasa.gov/planetary/apod?api_key=7aew7vcw7rzahpdR8apwkGsAdyWI6Ji8zCE44ACD",
 	success: handleResult
 });
 
-function handleResult(result) {
-	document.body.style.backgroundImage = "url(" + result.url + ")";
-	document.body.style.backgroundRepeat = "no-repeat";
-	document.body.style.backgroundSize = "cover";
-};
+$(window).load( onLoad );
