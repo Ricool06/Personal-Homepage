33c58f9b106942621ca60bf7c32995a63cc3a63c
Ryan Perry <ricoolohsix@hotmail.com>
2015-09-26 18:53:54 +0100

    +Added code to get average colour of the background image and change the title bar to that colour. +Gets CORS compatible APOD image to pull pixel data from. +Wrapped the seconds of the clock in <seconds> which makes them half the size of minutes and hours.

--- js/test.js
+++ js/test.js
@@ -14,9 +14,11 @@ function handleApodResult(result) {
 	xhr.onreadystatechange = function() {
 		if(xhr.readyState == 4 && xhr.status == 200) {
 				setBg(maxSizeImgUrl);
+				getDominantColorFromBg(imgUrl);
 		}
 		else{
 			setBg(imgUrl);
+			getDominantColorFromBg(imgUrl);
 		}
 	};
 };
@@ -51,7 +53,46 @@ function normalizeTime(h,m,s) {
 	if(s < 10) {
 		s = "0"+s;
 	}
-	return ""+h+":"+m+":"+s;
+	return ""+h+":"+m+"<seconds>:"+s+"</seconds>";
+}
+
+function getDominantColorFromBg() {
+	console.log(["DOMCOLOR","workpls"]);
+	invisImg = new Image;
+	c = document.createElement("canvas");
+	ctx = c.getContext("2d");
+	
+	invisImg.crossOrigin = "Anonymous";
+	//Must set this before setting invisImg src otherwise, we get a cross-origin error.
+	//Cross-origin error is when a local script tries to access potentially sensitive data on a remote DOM.
+	//CORS (cross origin resource sharing) has been enabled for APOD for anyone.
+	//Therefore, as long as invisImg's crossOrigin request is set to something, even Anonymous, it will work.
+		
+	invisImg.src = "https://api.nasa.gov/planetary/apod/direct?&api_key=7aew7vcw7rzahpdR8apwkGsAdyWI6Ji8zCE44ACD";
+	invisImg.onload = function(){
+		c.width = invisImg.width;
+		c.height = invisImg.height;
+		ctx.drawImage(invisImg,0,0);
+		imgData = ctx.getImageData(1, 1, c.width, c.height);
+		
+		primaryColours = [0,0,0,0];
+	
+		for(i = 0; i < (imgData.data.length); i+=4){
+			primaryColours[0] += imgData.data[i+0];
+			primaryColours[1] += imgData.data[i+1];
+			primaryColours[2] += imgData.data[i+2];
+			primaryColours[3] += imgData.data[i+3];
+		}
+		colourComponentAmount = imgData.data.length / 4;
+		redAverage = Math.round(primaryColours[0] / colourComponentAmount);
+		greenAverage = Math.round(primaryColours[1] / colourComponentAmount);
+		blueAverage = Math.round(primaryColours[2] / colourComponentAmount);
+		alphaAverage = Math.round(primaryColours[3] / colourComponentAmount);
+		
+		console.log(""+redAverage+","+greenAverage+","+blueAverage+","+alphaAverage);
+		
+		document.getElementById("centre-title").style.backgroundColor = 'rgba('+redAverage+','+greenAverage+','+blueAverage+',0.6)';
+	};
 }
 
 $.ajax({
@@ -60,3 +101,4 @@ $.ajax({
 });
 
 $(window).load( onLoad );
+
