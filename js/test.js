9669811f74b2fd1267d0b9c9b6281f15bbd55ace
Ryan Perry <ricoolohsix@hotmail.com>
2015-09-26 14:54:01 +0100

    +Script now tries to retrieve the high resolution (width of 2048) version of the current APOD image first. If that 404's, then it will retrieve the lower resolution image.

--- js/test.js
+++ js/test.js
@@ -2,21 +2,32 @@
  * @author Ryan Perry
  */
 function handleResult(result) {
-	maxSizeImgUrl = result.url.replace("1024","");
-	document.body.style.backgroundImage = "url(" + maxSizeImgUrl + ")";
-	document.body.style.backgroundRepeat = "no-repeat";
-	document.body.style.backgroundAttachment = "fixed";
-	document.body.style.backgroundSize = "cover";
-	document.body.style.animationName = "bg-transform";
-	document.body.style.animationDuration = "60s";
-	document.body.style.animationIterationCount = "infinite";
+	imgUrl = result.url;
+	maxSizeImgUrl = imgUrl.replace("1024","2048");
+	xhr = new XMLHttpRequest();
+	xhr.open("GET", maxSizeImgUrl, true);
+	xhr.send();
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState == 4 && xhr.status == 200) {
+				setBg(maxSizeImgUrl);
+		}
+		else{
+			setBg(imgUrl);
+		}
+	};
 };
 
 function onLoad() {
 	
 }
-function animateBg() {
-	
+function setBg(url) {
+	document.body.style.backgroundImage = "url(" + url + ")";
+	document.body.style.backgroundRepeat = "no-repeat";
+	document.body.style.backgroundAttachment = "fixed";
+	document.body.style.backgroundSize = "cover";
+	document.body.style.animationName = "bg-transform";
+	document.body.style.animationDuration = "120s";
+	document.body.style.animationIterationCount = "infinite";
 }
 $.ajax({
 	url:"https://api.nasa.gov/planetary/apod?api_key=7aew7vcw7rzahpdR8apwkGsAdyWI6Ji8zCE44ACD",
