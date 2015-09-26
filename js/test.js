0bc690a9e207ecc54efb941857a959640fe90e04
Ryan Perry <ricoolohsix@hotmail.com>
2015-09-26 15:22:28 +0100

    +Set up clock in the centre of the screen. +Added function to normalize time. (Add 0's onto the start of single digit hours, minutes or seconds.)

--- js/test.js
+++ js/test.js
@@ -1,7 +1,11 @@
 /**
  * @author Ryan Perry
  */
-function handleResult(result) {
+function onLoad() {
+	clock(document.getElementById("clock"));
+}
+
+function handleApodResult(result) {
 	imgUrl = result.url;
 	maxSizeImgUrl = imgUrl.replace("1024","2048");
 	xhr = new XMLHttpRequest();
@@ -17,9 +21,6 @@ function handleResult(result) {
 	};
 };
 
-function onLoad() {
-	
-}
 function setBg(url) {
 	document.body.style.backgroundImage = "url(" + url + ")";
 	document.body.style.backgroundRepeat = "no-repeat";
@@ -29,9 +30,33 @@ function setBg(url) {
 	document.body.style.animationDuration = "120s";
 	document.body.style.animationIterationCount = "infinite";
 }
+
+function clock(clockElement) {
+	date = new Date();
+	hour = date.getHours();
+	minute = date.getMinutes();
+	second = date.getSeconds();
+	time = normalizeTime(hour,minute,second);
+	clockElement.innerHTML = time;
+	setTimeout(function(){ clock(clockElement); }, 1000);
+}
+
+function normalizeTime(h,m,s) {
+	if(h < 10) {
+		h = "0"+h;
+	}
+	if(m < 10) {
+		m = "0"+m;
+	}
+	if(s < 10) {
+		s = "0"+s;
+	}
+	return ""+h+":"+m+":"+s;
+}
+
 $.ajax({
 	url:"https://api.nasa.gov/planetary/apod?api_key=7aew7vcw7rzahpdR8apwkGsAdyWI6Ji8zCE44ACD",
-	success: handleResult
+	success: handleApodResult
 });
 
 $(window).load( onLoad );
